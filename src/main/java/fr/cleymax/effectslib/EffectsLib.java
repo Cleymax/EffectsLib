04705d65cb422cbb0124c312d588f36d83642185
Clément P. (Cleymax) <clemperrin1@gmail.com>
2019-08-13 23:43:08 +0200

    :tada: Initial Commit

---
+++ src/main/java/fr/cleymax/effectslib/EffectsLib.java
@@ -0,0 +1,25 @@
+package fr.cleymax.effectslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EffectsLib extends JavaPlugin
+{
+	
+	@Override
+	public void onLoad()
+	{
+	
+	}
+	
+	@Override
+	public void onEnable()
+	{
+	
+	}
+	
+	@Override
+	public void onDisable()
+	{
+	
+	}
+}
